3224e29491a52d8bbe1701a912b7cf8bae3610f4
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-27 22:14:16 +0600

    added profile card

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -6,9 +6,19 @@ const ProfileCard = () => {
     const {profiles}=useContext(Context);
     console.log(profiles);
     return (
-        <div>
-            <h3>This profile card page , I do not want you see here.</h3>
-            <img src={profiles.img} alt="" />
+        <div className="bg-gray-400 p-20 m-20 rounded-lg flex">
+            <img className="w-2/4 rounded-lg" src={profiles.img} alt="" />
+            <div className="p-16">
+                <h3>Name:<span className="text-lg font-semibold">{profiles.name}</span> </h3>
+                <h3>Home-Town: <span className="text-lg font-semibold">{profiles.home}</span> </h3>
+                <p>Gender: {profiles.gendar} </p>
+                <p>Profession <span className="text-md font-semibold"> {profiles.profession}</span> </p>
+                <p>Hobby : <span className="text-md font-semibold"> {profiles.hobby}</span> </p>
+                <p>Institute : <span className="text-md font-semibold"> {profiles.institute}</span> </p>
+                <p>Blod : <span className="text-md font-semibold"> {profiles.blod}</span> </p>
+                <p>Department : <span className="text-md font-semibold"> {profiles.Department}</span> </p>
+                <p>{profiles.bio}</p>
+            </div>
         </div>
     );
 };
